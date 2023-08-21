d85384a7be559a2ddc1653b6cc623751907cb99e
Ivana Piscitelli <ivanap.ipiscitelli@gmail.com>
2023-08-21 12:32:23 +0200

    WIP: fixed size and color arrow slider

--- src/components/ButtonSlider.tsx
+++ src/components/ButtonSlider.tsx
@@ -1,6 +1,7 @@
 import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from "react-icons/md";
 
 import "../styles/index.css";
+import { Box, IconButton, useBreakpointValue } from "@chakra-ui/react";
 
 interface Props {
   direction: string;
@@ -8,14 +9,32 @@ interface Props {
 }
 
 const BtnSlider = ({ direction, moveSlide }: Props) => {
+  const iconSize = useBreakpointValue({ base: "30px", md: "70px" });
+  const iconColor = useBreakpointValue({ base: "black", md: "white", xl: "black" });
   return (
-    <button onClick={moveSlide} className={direction === "next" ? "btn-slide next" : "btn-slide prev"}>
+    <Box onClick={moveSlide} className={direction === "next" ? "btn-slide next" : "btn-slide prev"}>
       {direction === "next" ? (
-        <MdOutlineArrowForwardIos size={{ base: "19px", md: "30px" }} />
+        <IconButton
+          aria-label="arrow-forward"
+          icon={<MdOutlineArrowForwardIos size={iconSize} color={iconColor} />}
+          bgColor="transparent"
+          mr={{ base: "30px", sm: "50px", md: "30px", lg: "90px" }}
+          _active={{ bgColor: "transparent" }}
+          _hover={{ bgColor: "transparent" }}
+          _focus={{ boxShadow: "none", bgColor: "transparent" }}
+        />
       ) : (
-        <MdOutlineArrowBackIos size={{ base: "19px", md: "30px" }} />
+        <IconButton
+          aria-label="arrow-backward"
+          icon={<MdOutlineArrowBackIos size={iconSize} color={iconColor} />}
+          bgColor="transparent"
+          ml={{ base: "30px", sm: "50px", md: "30px", lg: "90px" }}
+          _active={{ bgColor: "transparent" }}
+          _hover={{ bgColor: "transparent" }}
+          _focus={{ boxShadow: "none", bgColor: "transparent" }}
+        />
       )}
-    </button>
+    </Box>
   );
 };
 
